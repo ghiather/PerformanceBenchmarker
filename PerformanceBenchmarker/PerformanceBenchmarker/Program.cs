69194c42c2efda5d5bf6351c4c35a7a9e2ad3348
Ghiath <ghiatherksoussi@gmail.com>
2019-11-25 13:52:20 -0500

    Cleaned up main function

--- PerformanceBenchmarker/PerformanceBenchmarker/Program.cs
+++ PerformanceBenchmarker/PerformanceBenchmarker/Program.cs
@@ -6,19 +6,11 @@ namespace PerformanceBenchmarker
 {
     class Program
     {
-        private static IList<long> DataTableResults, FastMemberResults, DapperResults, DataReaderResults;
-
         static void Main(string[] args)
         {
             Console.WriteLine("Performance Benchmarker V1.0 \n");
-            DataTableResults = new List<long>();
-            FastMemberResults = new List<long>();
-            DapperResults = new List<long>();
-            DataReaderResults = new List<long>();
-
             Startup testExcuter = new Startup();
             testExcuter.Excute(3);
-
             Console.ReadKey();
         }
     }
